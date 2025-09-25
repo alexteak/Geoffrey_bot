2c7ffdc3d1a40d5ef311152f2aafec85f5b079f8
Alexteak <getkreep@gmail.com>
2025-09-25 14:22:59 +0300

    upd

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const bot = new TApi(token, {polling: true});
 
 const answers = ["NO", "NO!", "NoOOooOOo", "Die a violent death", "Not a chance", "I did it", "What's a sandwich?"]
 
+let looper = setInterval(function(){
+    bot.sendMessage(1210911861, "awaiting messages")
+}, 10000);
+
 bot.on("message", msg => {
     console.log("get")
     const text = msg.text;
     const chatId = msg.chat.id;
     const chatname = msg.chat.title;
+    const msgId = msg.message_id;
     let words = text.split(" ")
     if(words.includes("make") && (words.includes("sandwich") || words.includes("sandwich?"))){
         bot.sendMessage(chatId, answers[Math.floor(Math.random()*answers.length)])
         console.log("responded")
     }
+    if(words.includes("infobruh")){
+        bot.sendMessage(chatId, chatId)
+        console.log("responded")
+    }
+    if(chatId == 1210911861){
+        setTimeout(function(){
+        bot.deleteMessage(chatId,msgId)}, 3000)
+    }
     }
 )
-
-
-
